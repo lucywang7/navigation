382ff17cd8c2bb8a961512756ba73123acf674fc
Lucy <lucywang340@gmail.com>
2025-07-10 09:38:45 -0500

    Initial Commit

---
+++ navigation/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  navigation
+//
+//  Created by Scholar on 7/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ navigation/navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationApp.swift
+//  navigation
+//
+//  Created by Scholar on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
